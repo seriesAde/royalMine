f1b1d6d106bc02cba1aa4a891e0e12241bfe80d4
Adeshina Ahmed <whygee007@gmail.com>
2026-02-17 17:57:14 +0100

    completed most of home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import { faFacebook, faFacebookF, faInstagramSquare, faLetterboxd, faSquareLetterboxd, faTiktok, faTwitter, faXTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram"
+import { faAngleRight, faEnvelope, faMapLocation, faPhone, faX } from "@fortawesome/free-solid-svg-icons"
+import { faLocation } from "@fortawesome/free-solid-svg-icons/faLocation"
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons/faLocationPin"
+import { faLocationPinLock } from "@fortawesome/free-solid-svg-icons/faLocationPinLock"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
+
+function Footer() {
+    return (
+        <div className="mt-10 bg-[#D8E6FD] ">
+            <div className="w-[85%] mx-auto">
+                <div className="flex capitalize pt-10">
+                    <ul className="text-[#0F2E4F] w-full leading-7">
+                        <h1 className="text-[#352F29] font-bold text-2xl mb-5">contact us</h1>
+                        <li className=" flex gap-2"><FontAwesomeIcon icon={faLocationPinLock} /> <p>Plot 251/252 Herbert Macaulay way Millennium Plaza Central Business District Opposite NNPC Towers.</p></li>
+                        <li className="flex items-center gap-2"><FontAwesomeIcon icon={faPhone} /> <p>+234 915 241 5476</p></li>
+                        <li className="flex items-center gap-2"> <FontAwesomeIcon icon={faEnvelope} /> <p>info@royalminesproperty.com</p></li>
+                    </ul>
+                    <ul className="text-[#0F2E4F] w-full leading-7">
+                        <h1 className="text-[#352F29] font-bold text-2xl mb-5">Quick Links</h1>
+                        <li className="hover:tracking-widest transition-all duration-500 ease-in-out text-md"><FontAwesomeIcon icon={faAngleRight} /><Link to="about">about us</Link></li>
+                        <li className="hover:tracking-widest transition-all duration-500 ease-in-out text-md"><FontAwesomeIcon icon={faAngleRight} /><Link to="contact">contact us</Link></li>
+                        <li className="hover:tracking-widest transition-all duration-500 ease-in-out text-md"><FontAwesomeIcon icon={faAngleRight} /><Link to="properties">our properties</Link></li>
+                        <li className="hover:tracking-widest transition-all duration-500 ease-in-out text-md"><FontAwesomeIcon icon={faAngleRight} /><Link to="/faq">frequently asked questions</Link></li>
+                    </ul>
+                </div>
+                <div className="flex gap-2 mt-5">
+                    <Link className="h-10 w-10 border border-gray-500 text-blue-900 hover:bg-[#0f2e4f] hover:text-white transition-all duration-700 ease-in-out rounded-full flex justify-center items-center bg-white"><FontAwesomeIcon className="text-lg" icon={faXTwitter} /></Link>
+                    <Link className="h-10 w-10 border border-gray-500 text-blue-900 hover:bg-[#0f2e4f] hover:text-white transition-all duration-700 ease-in-out rounded-full flex justify-center items-center bg-white"><FontAwesomeIcon className="text-lg" icon={faFacebookF} /></Link>
+                    <Link className="h-10 w-10 border border-gray-500 text-blue-900 hover:bg-[#0f2e4f] hover:text-white transition-all duration-700 ease-in-out rounded-full flex justify-center items-center bg-white"><FontAwesomeIcon className="text-lg" icon={faInstagram} /></Link>
+                    <Link className="h-10 w-10 border border-gray-500 text-blue-900 hover:bg-[#0f2e4f] hover:text-white transition-all duration-700 ease-in-out rounded-full flex justify-center items-center bg-white"><FontAwesomeIcon className="text-lg" icon={faTiktok} /></Link>
+                </div>
+                <div className="flex justify-between mt-20 pb-5">
+                    <p className="font-light text-sm">© 2026- <Link to="/" className="text-[#6EB5FF]">RoyalMines Properties</Link></p>
+                    <div className="flex gap-5 text-[#6EB5FF]">
+                        <Link to="/" className="border-r border-gray-400 pr-5">Home</Link>
+                        <Link to="/faq">FAQ</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} export default Footer
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav"
+
+function About() {
+    return (
+        <div>
+            <Nav />
+            <h1>this is about page</h1>
+        </div>
+    )
+}
+export default About
